378d412f2301d597a03751c4847c2e6e647942d5
dylannegri <negrm581@newschool.edu>
2017-02-13 16:32:43 +0100

    2/13/17 update

--- js/main.js
+++ js/main.js
@@ -18,6 +18,19 @@ app.main = (function() {
 		$(this).css({'color': 'white'},50);
 	});
 
+	$(document).ready(function () {
+		// console.log("ready!");
+		$("#skill01").animate({"width": "63%"},1000);
+		$("#skill02").animate({"width": "57%"},1250);
+        $("#skill03").animate({"width": "60%"},1500);
+        $("#skill04").animate({"width": "90%"},1750);
+        $("#skill05").animate({"width": "72%"},2000);
+        $("#skill06").animate({"width": "65%"},2250);
+        $("#skill07").animate({"width": "84%"},2500);
+        $("#skill08").animate({"width": "55%"},2750);
+        $("#skill09").animate({"width": "100%"},3000);
+    });
+
 	var init = function(){
 		console.log('Initializing app.');
 		
